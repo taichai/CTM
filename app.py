4b8cbae51a1f9a4d6b7558995927c677d6f14f9a
osboxes.org <taichai@live.hk>
2022-12-10 16:25:47 +0000

    v3.1

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Docker!'
+    return 'Hello, myDocker3!\nThis is version3.1'
 
 if __name__== '__main__':
     app.run(host="0.0.0.0", port=5000, threaded=True, debug=True)
